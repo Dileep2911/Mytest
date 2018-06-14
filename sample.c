6eec07a315ff39b2a986c7c11e69fd196727c24a
Dileep2911 <38869776+Dileep2911@users.noreply.github.com>
2018-06-14 20:04:28 +0530

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,13 +1,6 @@
-#!include<stdio.h>
+#include<stdio.h>
 int main()
 {
-  
-  sshagent(['tomcat-dev']) {
-
-sh 'scp -o StrictHostKeyChecking=no target/*jar ec2-user@34.217.105.21:8080/manager/'
-
-}
-  
 printf("hello world");
 return 0;
 }
